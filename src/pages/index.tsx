7e9483dc75c462f763fcdc1941737b34bc07be31
Evander Inacio <evander.20112@hotmail.com>
2022-08-27 10:54:49 -0300

    Initial Commit :rocket:

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      hello world
+    </div>
+  )
+}
+
+export default Home
---
+++ src/styles/global.ts
@@ -0,0 +1,10 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+})
