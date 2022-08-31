e9966ac78c9263770576ff0ca87b31e0d463b180
Evander Inacio <evander.20112@hotmail.com>
2022-08-31 11:45:27 -0300

    Fonts :pencil2:

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,13 +6,10 @@ export default class CustomDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-          <style id="stitches" dangerouslySetInnerHTML={{__html: getCssText()}}/>
+          <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet" />
+          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
         </Head>
 
         <body>
